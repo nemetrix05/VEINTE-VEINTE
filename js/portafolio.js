213b1e431fb198fe135068e388d2814368dbb22a
Andres cruz <cgilmarandres@gmail.com>
2018-11-20 20:57:32 -0500

    Veinte Veinte Base

---
+++ js/portafolio.js
@@ -0,0 +1,216 @@
+// Textos descripcion Portafolio
+
+// 1. CÁMARA DE COMERCIO ITALIANA PARA COLOMBIA 
+
+function Camara () {
+    return(
+        {
+
+            'descripcion'   :   'Nueva imagen corporativa para la Cámara de Comercio Italiana para Colombia, entidad que supera los 60 años de trayectoria, ofreciendo y promoviendo oportunidades de negocios e inversión.',
+
+            'descripcion2'   :   'Diseño editorial e infográfico para el programa “Estudia en Italia” de la Cámara de Comercio Italiana para Colombia, ofreciendo una amplia variedad de oportunidades de estudio en distintas ciudades y escuelas de Italia.',  
+
+            'descripcion3'   :   'Desarrollo de imagen, gráfica, panelería y papelería para el evento “Aula Italia” organizado por la Cámara de Comercio Italiana para Colombia. Un escenario para estudiantes y ejecutivos en búsqueda de oportunidades de estudio en Italia. Fotos cortesía de la Cámara de Comercio de Bogotá.'                         
+        }
+    )
+}
+
+$('.producto1 .p01').html(Camara().descripcion);
+$('.producto1 .p02').html(Camara().descripcion2);
+$('.producto1 .p03').html(Camara().descripcion3);
+
+// 2. VEINTE-VEINTE 
+
+function VeinteVeinte () {
+    return(
+        {
+            'descripcion'   :   'Echa un vistazo a nuestros últimos artworks, productos y diseños; ofrecemos una gran cantidad de variedad gráfica aplicando diferentes técnicas, ofreciendo soluciones nuevas y creativas a tus necesidades.',
+            
+            'descripcion2'   :   'Para aquellas personas que aman viajar desafiando todo pronóstico y disfrutando cada kilómetro de la ruta, presentamos nuestras #wanderbook, la herramienta adecuada para registrar tus pasos y ampliar tus destinos.',
+            
+            'descripcion3'   :   'Mi vieja, mi amiga, mi madre, mi todo. Gracias por darme el regalo más grande que puede haber en el mundo; la vida, y más aún, por acompañarme en cada paso que doy en ella. Por esto y mucho más, todos los días son tu día.',
+            
+            'descripcion4'   :   'Viernes, 20 de abril.',
+            
+            'descripcion5'   :   'Ya son 293 afectados por pólvora este 2017 luego de la noche buena. Es momento de cambiar; que la paz signifique tranquilidad, por eso #MeComprometo #HaréPaz #hARÉPAz #Arepas #ArePeace.',
+            
+            'descripcion6'   :   'Seguimos creciendo gracias a su apoyo.'             
+        }
+    )
+}
+
+$('.producto2 .p01').html(VeinteVeinte().descripcion);
+$('.producto2 .p02').html(VeinteVeinte().descripcion2);
+$('.producto2 .p03').html(VeinteVeinte().descripcion3);
+$('.producto2 .p04').html(VeinteVeinte().descripcion4);
+$('.producto2 .p05').html(VeinteVeinte().descripcion5);
+$('.producto2 .p06').html(VeinteVeinte().descripcion6);
+
+// 3. ISTITUTO ITALIANO DI CULTURA BOGOTÁ
+
+
+function InstItaliano () {
+    return(
+        {
+
+            'descripcion'   :   'En el corazón de Teusaquillo en Bogotá, se vive Italia a través de la cultura.',
+
+            'descripcion2'   :   'Brochure tríptico informativo sobre opciones de estudio, clases y tutorías del idioma italiano en el Istituto Italiano di Cultura Bogotá.',  
+
+            'descripcion3'   :   'Poster para el evento/festival cinematográfico “Fare Cinema” donde se llevó a cabo una muestra y un foro sobre cine italiano.',
+            
+            'descripcion4'   :   'Diseño de pendones y comunicaciones a partir de la estructura del logo existente, generando un lineamiento gráfico coherente y un branding contundente.'            
+        }
+    )
+}
+
+$('.producto3 .p01').html(InstItaliano().descripcion);
+$('.producto3 .p02').html(InstItaliano().descripcion2);
+$('.producto3 .p03').html(InstItaliano().descripcion3);
+$('.producto3 .p04').html(InstItaliano().descripcion4);
+
+
+// 4. MAGONZAR
+
+
+function Mangonzar () {
+    return(
+        {
+
+            'descripcion'   :   'Marca personal/corporativa para una entidad dedicada a la asesoría en contratación estatal.'         
+        }
+    )
+}
+
+$('.producto4 .p01').html(Mangonzar().descripcion);
+
+
+// 5. SANTOS DISCEPOLO
+
+function SantosDicepolo () {
+    return(
+        {
+
+            'descripcion'   :   'Imagen para los amigos del centro cultural + bar + comedor popular + etc: Santos Discepolo. San Miguel de Tucumán, Argentina.'         
+        }
+    )
+}
+
+$('.producto5 .p01').html(SantosDicepolo().descripcion);
+
+// 6. LIGHT & LIGERO
+
+function LightLigero () {
+    return(
+        {
+
+            'descripcion'   :   'Id. personal para una especial repostería y pastelería que ofrece productos sin azúcar en San Juan del Cesar, La Guajira.'         
+        }
+    )
+}
+
+$('.producto6 .p01').html(LightLigero().descripcion);
+
+// 7. PISPESCA
+
+function Pispesca () {
+    return(
+        {
+
+            'descripcion'   :   'Opción para imagen corporativa de la Asociación de Piscicultura y Pesca, PISPESCA.'         
+        }
+    )
+}
+
+$('.producto7 .p01').html(Pispesca().descripcion);
+
+
+// 8. ALMA PARRANDERA
+
+function AlmaParrandera () {
+    return(
+        {
+
+            'descripcion'   :   'Propuesta para identificador y look de la cuenta de Instagram @almaparrandera.'         
+        }
+    )
+}
+
+$('.producto8 .p01').html(AlmaParrandera().descripcion);
+
+
+// 9. STONE SYNCHRONIZE
+
+function StoneSyn () {
+    return(
+        {
+
+            'descripcion'   :   'Diseño de ID Visual + Rebranding para emprendimiento de producción musical, asesoría de imagen, creación y distribución de contenidos en distintos canales y redes.', 
+            
+            'descripcion2'   :   'Cartel para la fiesta/recital Party Time, en Casa Valhalla, Bogotá.',     
+            
+            'descripcion3'   :   'Cartel para la apertura del Bogotá Music Video Festival 2017, en Smoking Molly, Bogotá.'            
+        }
+    )
+}
+
+$('.producto9 .p01').html(StoneSyn().descripcion);
+$('.producto9 .p02').html(StoneSyn().descripcion2);
+$('.producto9 .p03').html(StoneSyn().descripcion3);
+
+// 10. TERRY SURF
+
+function TerrySurf () {
+    return(
+        {
+
+            'descripcion'   :   'Marca personal para un emprendimiento de clases de surf y reparación de tablas en Huanchaco, Perú.'         
+        }
+    )
+}
+
+$('.producto10 .p01').html(TerrySurf().descripcion);
+
+// 11. FRIENDS FOREVER
+
+function OtrosProyectos () {
+    return(
+        {
+            'descripcion'   :   '<strong>GUAJIRO NATURAL:</strong><br /> El 3 de octubre cumple años el tipo más auténtico que podrían conocer. Legítimo de su tierra, de genuinas costumbres y carácter sin igual. Pilar de un hogar y una familia. Y por fortuna, mi papá.',
+            
+            'descripcion2'   :   '<strong>STICKERS</strong><br />',
+            
+            'descripcion3'   :   '<strong>PEDRO DEL SON:</strong><br /> Visual para nuestro estimado Pedro del Son interpretando su actitud y propuesta musical a través de la mezcla de elementos tropicalidosos en un contexto galo-urbano.',
+
+            'descripcion4'   :   '<strong>LAS NOTAS DE JUANCHO:</strong><br /> Diseño y desarrollo gráfico para la promoción de este evento que busca darle nuevos aires a la propuesta musical del difunto, y para muchos el mejor acordionista del folclor vallenato, Juancho Rois.',
+
+            'descripcion5'   :   '<strong>PURA VIDA:</strong><br /> Diseño de ID Visual + Branding para finca auto sostenible y ecológica con fines turísticos y de esparcimiento cultural en el Magdalena.',
+            
+            'descripcion6'   :   '<strong>THE YOUTH SHALL RISE:</strong><br /> Cartel para los amigos del fanzine de punk, Plagio.',   
+            
+            'descripcion7'   :   '<strong>SAPOFEST:</strong><br /> Cartel para el festival clandestino de cumbia Sapofest en Rosario, Argentina.',    
+            
+            'descripcion8'   :   '<strong>NIUMUN:</strong><br /> Diseño de ID Visual + Rebranding + Collage para una increíble fiesta de fin de año en Concón, Viña del Mar.',   
+            
+            'descripcion9'   :   '<strong>DON OSVALDO:</strong><br /> Flyer para el regreso de una legendaria banda argentina bajo un nuevo nombre y nuevos shows a lo largo del país.',   
+            
+            'descripcion10'   :   '<strong>THIS IS ‘RAVE!:</strong><br /> Artwork para la promoción de la serie británica "This is England 90.',   
+            
+            'descripcion11'   :   '<strong>KÜTRAL:</strong><br /> Arte Visual + Collage para fiesta temática en Concón, Viña del Mar.'            
+            
+        }
+    )
+}
+
+
+$('.producto12 .p01').html(OtrosProyectos().descripcion);
+$('.producto12 .p02').html(OtrosProyectos().descripcion2);
+$('.producto12 .p03').html(OtrosProyectos().descripcion3);
+$('.producto12 .p04').html(OtrosProyectos().descripcion4);
+$('.producto12 .p05').html(OtrosProyectos().descripcion5);
+$('.producto12 .p06').html(OtrosProyectos().descripcion6);
+$('.producto12 .p07').html(OtrosProyectos().descripcion7);
+$('.producto12 .p08').html(OtrosProyectos().descripcion8);
+$('.producto12 .p09').html(OtrosProyectos().descripcion9);
+$('.producto12 .p010').html(OtrosProyectos().descripcion10);
+$('.producto12 .p011').html(OtrosProyectos().descripcion11);
